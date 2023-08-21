c9d48d807f9833f8436695126dd9937a2cb6c863
Anup Kharwar <103813332+AnupKharwar@users.noreply.github.com>
2023-08-21 19:45:32 +0530

    Update streamlit_app.py
    Menu Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.header("Todays Menu")
+streamlit.test("Dal and Rice")
+streamlit.test("fish and Rice")
+streamlit.test("Utpam")
+
