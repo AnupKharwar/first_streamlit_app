0b3abb3cdc5ea16778a2b253a2b13d6e3fa8f318
Anup Kharwar <103813332+AnupKharwar@users.noreply.github.com>
2023-08-21 19:47:45 +0530

    Update streamlit_app.py
    Added BreakFast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
-streamlit.header("Todays Menu")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header("Todays Lunch Menu")
 streamlit.text("Dal and Rice")
 streamlit.text("fish and Rice")
 streamlit.text("Utpam")
-
