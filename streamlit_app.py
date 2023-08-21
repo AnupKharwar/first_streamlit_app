33486c3b8e8875e3ec41810cc6266eccbdec6728
Anup Kharwar <103813332+AnupKharwar@users.noreply.github.com>
2023-08-21 19:46:14 +0530

    Update streamlit_app.py
    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header("Todays Menu")
-streamlit.test("Dal and Rice")
-streamlit.test("fish and Rice")
-streamlit.test("Utpam")
+streamlit.text("Dal and Rice")
+streamlit.text("fish and Rice")
+streamlit.text("Utpam")
 
